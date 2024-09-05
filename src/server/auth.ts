6430dd7affdf32934351c5891c01588e8bc8d186
moh3a <aitabdelmalekma@gmail.com>
2024-09-05 22:39:49 +0100

    Add next auth drizzle adapter

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,19 +1,19 @@
-// import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import {
   getServerSession,
   type DefaultSession,
   type NextAuthOptions,
 } from "next-auth";
-// import { type Adapter } from "next-auth/adapters";
+import { type Adapter } from "next-auth/adapters";
 
 import { env } from "~/env";
-// import { db } from "~/server/db";
-// import {
-//   accounts,
-//   sessions,
-//   users,
-//   verificationTokens,
-// } from "~/server/db/schema";
+import { db } from "~/server/db";
+import {
+  accounts,
+  sessions,
+  users,
+  verificationTokens,
+} from "~/server/db/schema";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -46,12 +46,12 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   },
-  // adapter: DrizzleAdapter(db, {
-  //   usersTable: users,
-  //   accountsTable: accounts,
-  //   sessionsTable: sessions,
-  //   verificationTokensTable: verificationTokens,
-  // }) as Adapter,
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+    verificationTokensTable: verificationTokens,
+  }) as Adapter,
   providers: [
     {
       id: "aliexpress",
